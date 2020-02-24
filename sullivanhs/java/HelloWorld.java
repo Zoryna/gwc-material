64878cce089ac748d1d038de2e859b16396bd686
Zoryna <jsoryna@gmail.com>
2020-02-24 12:16:22 -0600

    First Java file Hello World

---
+++ sullivanhs/java/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+Learn how to code print statements
+in Java
+*/
+
+public class HelloWorld
+{
+  public static void main (String[] args)
+  {
+    //this is how you write one-line comments in Java
+
+    /*
+    This is how you write comments that are more than
+    one line
+    Comments don't affect your code, they just help coders
+    read the code easier
+    */
+
+    System.out.println("Hello, world!");
+    System.out.println("This is how you code a print statement in Java");
+    System.out.println(""); //TODO enter your own message
+  }
+}
